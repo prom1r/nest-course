aaaeb04a16aebd5a44c659246f1ace5f32a417c7
d.toforov <d.toforov@itransition.com>
2025-03-04 14:21:44 +0300

    added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config'; // ConfigService
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,17 @@ async function bootstrap() {
   const configService = app.get(ConfigService); // ConfigService from nestjs
   const port = configService.get('port'); //get port
 
+  const swaggerConfig = new DocumentBuilder() //инициализация swagger
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig); //создание документа swagger
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
+
 bootstrap();
