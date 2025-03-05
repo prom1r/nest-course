3d2c3a54eb64d7761d0e2108ed14202d4c0e9792
d.toforov <toforovdima@yandex.ru>
2025-03-05 13:20:03 +0300

    added check for user existence

---
+++ backend/src/common/errors.ts
@@ -0,0 +1,4 @@
+export const AppErrors = {
+  USER_NOT_FOUND: 'User not found',
+  USER_ALREADY_EXISTS: 'User already exists',
+};
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import * as bcrypt from 'bcrypt';
 import { createUserDto } from './dto';
+import { AppErrors } from 'src/common/errors';
 
 @Injectable()
 export class UsersService {
@@ -16,10 +17,25 @@ export class UsersService {
     return bcrypt.hash(password, 10);
   }
 
-  async createUser(dto): Promise<createUserDto> {
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async createUser(dto: createUserDto): Promise<createUserDto> {
+    const existUser = await this.findUserByEmail(dto.email);
+
+    if (existUser) throw new BadRequestException(AppErrors.USER_ALREADY_EXISTS);
+
     dto.password = await this.hashPassword(dto.password);
 
-    await this.userRepository.create(dto);
+    const newUser = {
+      firstName: dto.firstName,
+      userName: dto.userName,
+      password: dto.password,
+      email: dto.email,
+    };
+
+    await this.userRepository.create(newUser);
     return dto;
   }
 }
